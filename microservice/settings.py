41888304af6c8262837e23793ec40ef190203192
Leskey <leskeylevy@gmail.com>
2022-03-06 12:05:29 +0300

    define secret_key in environment file and update settings to read from env file

--- microservice/settings.py
+++ microservice/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=i%%2kj(=d)3g&eevo7php8758%35ubw$s4)x&72qs6riya-v+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
